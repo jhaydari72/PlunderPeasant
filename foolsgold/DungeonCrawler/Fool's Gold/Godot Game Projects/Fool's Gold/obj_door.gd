5009b21809ccc203dd2caee1ddf311a150eac1d7
jhaydari72@gmail.com <jhaydari72@gmail.com>
2020-06-04 22:20:40 -0700

    door opens and collision disappears

--- foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/obj_door.gd
+++ foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/obj_door.gd
@@ -1,9 +1,11 @@
 extends StaticBody2D
 
+onready var sprite = $Sprite
+
 func _ready():
 	$Area2D.connect("body_entered",self,"body_entered")
 
 func body_entered(body):
 	if body.name == "obj_player" && body.get("Key") > 0:
 		body.Key -= 1
-		queue_free()
+		$Sprite.play("open")
