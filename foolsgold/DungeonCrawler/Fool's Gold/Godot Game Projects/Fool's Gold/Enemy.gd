98c20c41a99608aac1e9defa8fe641f6355177ad
jhaydari72@gmail.com <jhaydari72@gmail.com>
2020-06-07 13:58:04 -0700

    enemy wanders

--- foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/Enemy.gd
+++ foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/Enemy.gd
@@ -5,6 +5,7 @@ export var SPEED = 50
 export var FRICTION = 200
 enum {
 	IDLE,
+	WANDER,
 	DEAD,
 	CHASE
 }
@@ -16,20 +17,38 @@ var knockback = Vector2.ZERO
 onready var sprite = $Sprite
 onready var playerdetection = $PlayerDetection
 onready var hurtbox = $Hurtbox
-
+onready var controller = $Wander
 
 func _ready():
 	pass 
 	
 
 func _physics_process(delta):
+	
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	
 	match state:
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			$Sprite.play("idle")
 			find_player()
+			if controller.time_left() == 0:
+				state = new_state([IDLE, WANDER])
+				controller.start_wander_timer(rand_range(1, 3))
+		WANDER:
+			find_player()
+			if controller.time_left() == 0:
+				state = new_state([IDLE, WANDER])
+				controller.start_wander_timer(rand_range(1, 3))
+				
+			var direction = global_position.direction_to(controller.target_position)
+			velocity = velocity.move_toward(direction * SPEED, ACCELERATION * delta)
+			
+			if global_position.distance_to(controller.target_position) <= 10:
+				state = new_state([IDLE, WANDER])
+				controller.start_wander_timer(rand_range(1, 3))
+			
 		DEAD:
 			if is_dead == true:
 				velocity = Vector2(0, 0)
@@ -38,13 +57,16 @@ func _physics_process(delta):
 			var player = playerdetection.player
 			if player != null:
 				$Sprite.play("walk")
-				var direction = (player.global_position - global_position).normalized()
+				var direction = global_position.direction_to(player.global_position)
 				velocity = velocity.move_toward(direction * SPEED, ACCELERATION * delta)
 			else:
 				state = IDLE
 			sprite.flip_h = velocity.x > 0
 	velocity = move_and_slide(velocity)
 	
+func new_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
 		
 func find_player():
 	if playerdetection.can_see_player():
