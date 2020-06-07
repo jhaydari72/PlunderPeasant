0339c7dd705a2f09dc60c3664302ae0d7edde917
jhaydari72@gmail.com <jhaydari72@gmail.com>
2020-06-06 20:21:41 -0700

    knock back on test enemy working

--- foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/Enemy.gd
+++ foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/Enemy.gd
@@ -5,6 +5,8 @@ const SPEED = 50
 var velocity = Vector2()
 var direction = -1
 var is_dead = false
+var knockback = Vector2.ZERO
+
 onready var sprite = $Sprite
 
 
@@ -14,6 +16,8 @@ func _ready():
 	
 
 func _physics_process(delta):
+	knockback = knockback.move_toward(Vector2.ZERO, 500 * delta)
+	knockback = move_and_slide(knockback)
 	if is_dead == false:
 		velocity.x = SPEED * direction
 		if direction == 1:
@@ -28,7 +32,8 @@ func _physics_process(delta):
 		direction = direction * -1
 		
 
-func _on_Hurtbox_area_entered(_area):
+func _on_Hurtbox_area_entered(area):
+	knockback = area.knockback_vector * 200
 	is_dead = true
 	velocity = Vector2(0, 0)
 	$Sprite.play("dead")
