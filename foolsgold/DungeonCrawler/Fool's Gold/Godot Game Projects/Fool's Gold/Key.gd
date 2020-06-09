2661227c897f8ac72b7434c877f7a6fa54028561
jhaydari72@gmail.com <jhaydari72@gmail.com>
2020-06-08 21:11:03 -0700

    debugs and occlusion fix

--- foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/Key.gd
+++ foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/Key.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
-func _ready():
-	connect("body_entered", self, "body_entered")
 
-func body_entered(body):
+	
+
+
+func _on_Key_body_entered(body):
 	if body.name == "obj_player" && body.get("Key") < 9:
 		body.Key += 1
 		queue_free()
