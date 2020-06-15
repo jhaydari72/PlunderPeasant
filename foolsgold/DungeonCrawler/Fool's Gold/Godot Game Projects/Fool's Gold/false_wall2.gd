909bc008b175fd3fbed6629970395fcb97426505
jhaydari72@gmail.com <jhaydari72@gmail.com>
2020-06-14 17:05:38 -0700

    false wall working off of torches

--- foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/false_wall2.gd
+++ foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/false_wall2.gd
@@ -2,13 +2,17 @@ extends StaticBody2D
 
 #mOVING WALLS 
 onready var timer = $Timer
-
+onready var area = $Area2D
 
 func _ready():
 	pass 
 
 
-func _on_Area2D_area_entered(_area):
-	$AnimatedSprite.play("open")
-	timer.start()
+func _input(_event):
+	if Input.is_action_just_pressed("ui_enter"):
+		var bodies = $Area2D.get_overlapping_bodies()
+		for b in bodies:
+			if b.name == "obj_player":
+				$AnimatedSprite.play("open")
+				timer.start()
 
