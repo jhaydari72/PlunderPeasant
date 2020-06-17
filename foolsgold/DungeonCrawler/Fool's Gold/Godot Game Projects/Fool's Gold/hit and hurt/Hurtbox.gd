98e56d9b06761b00ddd8d21481397413a3b93048
jhaydari72@gmail.com <jhaydari72@gmail.com>
2020-06-16 19:18:36 -0700

    debug

--- foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/hit and hurt/Hurtbox.gd
+++ foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/hit and hurt/Hurtbox.gd
@@ -30,11 +30,13 @@ func _on_Timer_timeout():
 
 func _on_Hurtbox_invincibility_started():
 	set_deferred("monitorable", false)
+	invincible = false
 	
 
 
 func _on_Hurtbox_invincibility_ended():
 	set_deferred("monitorable", true)
+	invincible = true
 	
 
 
