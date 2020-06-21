0436ebba6f1aa3dd2807806289118cdf4fd1439b
Jose L. Santa Maria <j.guzman2320@yahoo.com>
2020-06-20 23:58:10 -0700

    Splash Screens 90% completed

---
+++ foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/Splash/Fool's Gold SplashScreen.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+var value = null
+
+
+func _ready():
+	pass 
+	
+
+func _on_Button_pressed():
+	value = get_tree().change_scene("res://Splash/GodotSplashScreen.tscn")
+	return value
+	
+
+
+func _on_Timer_timeout():
+	value = get_tree().change_scene("res://Splash/GodotSplashScreen.tscn")
+	return value
