cea5d6a2e35ac99ed46d90d5e2ed3232522e1632
Jose L. Santa Maria <j.guzman2320@yahoo.com>
2020-06-24 00:22:30 -0700

    Currently WOrking on Upper Left Level

--- foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/Splash/Fool's Gold SplashScreen.gd
+++ foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/Splash/Fool's Gold SplashScreen.gd
@@ -10,11 +10,11 @@ func _ready():
 	
 
 func _on_Button_pressed():
-	value = get_tree().change_scene("res://Splash/GodotSplashScreen.tscn")
+	value = get_tree().change_scene("res://Home Screen/HomeScreen.tscn")
 	return value
 	
 
 
 func _on_Timer_timeout():
-	value = get_tree().change_scene("res://Splash/GodotSplashScreen.tscn")
+	value = get_tree().change_scene("res://Home Screen/HomeScreen.tscn")
 	return value
--- foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/Splash/SAESplashScreen.gd
+++ foolsgold/DungeonCrawler/Fool's Gold/Godot Game Projects/Fool's Gold/Splash/SAESplashScreen.gd
@@ -11,11 +11,11 @@ func _ready():
 
 
 func _on_Button_pressed():
-	value = get_tree().change_scene("res://Home Screen/HomeScreen.tscn")
+	value = get_tree().change_scene("res://Splash/Fool's Gold SplashScreen.tscn")
 	return value
 
 
 
 func _on_Timer_timeout():
-	value = get_tree().change_scene("res://Home Screen/HomeScreen.tscn")
+	value = get_tree().change_scene("res://Splash/Fool's Gold SplashScreen.tscn")
 	return value
